1387551763ca6a3bf4aa36dc313fc2d90920b119
suiyujin <suiyujin04@gmail.com>
2016-03-12 22:23:47 +0900

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'csv'
+
+def make_dummy_data(data)
+  data[1..-1].map { |d| Hash[*data[0].zip(d).flatten] }
+end
+
+# products
+Product.create(make_dummy_data(CSV.read('db/seed_data/products.csv')))
+p 'products created.'
+
+# foods
+Food.create(make_dummy_data(CSV.read('db/seed_data/foods.csv')))
+p 'foods created.'
+
