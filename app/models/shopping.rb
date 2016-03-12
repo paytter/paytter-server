89a31ce6e8a948b6417fa559071e54cf5418945b
hoshitocat <hoshitocat@gmail.com>
2016-03-13 00:26:02 +0900

    Added relations

--- app/models/shopping.rb
+++ app/models/shopping.rb
@@ -1,4 +1,5 @@
 class Shopping < ActiveRecord::Base
   belongs_to :user
   belongs_to :product
+  belongs_to :store
 end
--- app/models/store.rb
+++ app/models/store.rb
@@ -2,4 +2,5 @@ class Store < ActiveRecord::Base
   has_many :product_stores
   has_many :products, through: :products
   has_many :purchases
+  has_many :shoppings
 end
