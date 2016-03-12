455b694d1ce2b5b7af514a58f96e5607ed24de10
hoshitocat <hoshitocat@gmail.com>
2016-03-13 06:29:01 +0900

    Added users_controller#create

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+
+  skip_before_action :authorized_access_token!
+
+  def create
+    @user = User.save_with_bank_accounts(create_params)
+    if @user.errors.blank?
+      render json: { access_token: @user.access_token }.to_json
+    else
+      render json: { errors: @user.errors.full_messages }
+    end
+  end
+
+  private
+
+  def create_params
+    params.require(:user).permit(
+      :user_id, :user_name, :post_code, :address, :phone_number, :email, :image,
+      my_accounts: []
+    )
+  end
+end
+
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,23 @@ class User < ActiveRecord::Base
 
   has_many :bank_accounts
   has_many :shoppings
+
+  def self.save_with_bank_accounts(params)
+    user = self.new(params.except(:my_accounts))
+    begin
+      self.transaction do
+        # TODO: 変更する
+        user.password = 'password'
+        params.slice(:my_accounts).map do |attrs|
+          user.bank_accounts.build(attrs)
+        end
+
+        user.access_token = SecureRandom.urlsafe_base64(24).tr('lIO0', 'sxyz')
+
+        user.save!
+      end
+    rescue ActiveRecord::RecordInvalid => invalid
+    end
+    user
+  end
 end
