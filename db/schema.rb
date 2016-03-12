da67c96edd6bd6222446ebe3c2be144bfe82ffe7
hoshitocat <hoshitocat@gmail.com>
2016-03-13 00:26:42 +0900

    Added access_token to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160312151132) do
+ActiveRecord::Schema.define(version: 20160312152328) do
 
   create_table "bank_accounts", force: :cascade do |t|
     t.integer  "user_id",                limit: 4
@@ -156,8 +156,10 @@ ActiveRecord::Schema.define(version: 20160312151132) do
     t.string   "last_sign_in_ip",        limit: 255
     t.datetime "created_at",                                      null: false
     t.datetime "updated_at",                                      null: false
+    t.string   "access_token",           limit: 255
   end
 
+  add_index "users", ["access_token"], name: "index_users_on_access_token", unique: true, using: :btree
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
